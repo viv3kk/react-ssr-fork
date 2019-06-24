7d3bea84810837c1555009c2326aec55cee86eb7
Vivek Prakash <vivek@Viveks-MacBook-Pro.local>
2019-06-25 00:34:41 +0530

    with lint

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,9 @@ class Home extends React.Component {
                 <h2>F1 2018 Season Calendar</h2>
                 <ul>
                     { circuits.map( ( { circuitId, circuitName, Location } ) => (
-                        <li key={ circuitId } >{ circuitName } - { Location.locality }, { Location.country }</li>
+                        <li key={ circuitId } >
+                            { circuitName } - { Location.locality }, { Location.country }
+                        </li>
                     ) ) }
                 </ul>
             </div>
